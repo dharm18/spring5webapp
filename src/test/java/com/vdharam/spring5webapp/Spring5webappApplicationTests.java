dd27adb9ae0bd138e64fbd57576f633f993ec359
dharm18 <vishdharm.181910@gmail.com>
2019-06-16 11:50:12 +0100

    First commit

---
+++ src/test/java/com/vdharam/spring5webapp/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vdharam.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
