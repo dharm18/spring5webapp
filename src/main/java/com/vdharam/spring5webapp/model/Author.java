2b811c147dce27e6aea9d320918e665002a34e37
dharm18 <vishdharm.181910@gmail.com>
2019-06-16 21:34:33 +0100

    Hashcode and Equals method for Set uniqueness

--- src/main/java/com/vdharam/spring5webapp/model/Author.java
+++ src/main/java/com/vdharam/spring5webapp/model/Author.java
@@ -58,4 +58,46 @@ public class Author {
     public void setBooks(Set<Book> books) {
         this.books = books;
     }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Author [id=");
+		builder.append(id);
+		builder.append(", firstName=");
+		builder.append(firstName);
+		builder.append(", lastName=");
+		builder.append(lastName);
+		builder.append(", books=");
+		builder.append(books);
+		builder.append("]");
+		return builder.toString();
+	}
+    
+    
 }
--- src/main/java/com/vdharam/spring5webapp/model/Book.java
+++ src/main/java/com/vdharam/spring5webapp/model/Book.java
@@ -81,4 +81,48 @@ public class Book {
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
     }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Book [id=");
+		builder.append(id);
+		builder.append(", title=");
+		builder.append(title);
+		builder.append(", isbn=");
+		builder.append(isbn);
+		builder.append(", publisher=");
+		builder.append(publisher);
+		builder.append(", authors=");
+		builder.append(authors);
+		builder.append("]");
+		return builder.toString();
+	}
+    
+    
 }
